ec0f72228f8aad356046f5d7b77d1facb6801b11
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-09-11 16:41:05 -0400

    clean up scripts

--- setup.bash
+++ setup.bash
@@ -4,8 +4,8 @@
 if [[ "$OSTYPE" == "darwin"* ]] ; then
   # install command line utils, homebrew, and python
   xcode-select --install
-  command -v brew
-  if [[ $? != 0 ]] ; then
+
+  if ! command -v brew &> /dev/null; then
       # Install Homebrew
       echo "Installing Homebrew."
       ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -25,7 +25,7 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] ; then
   touch "$HOME/.config/terminator/config"
 
   # Debian
-  if which apt-get &> /dev/null; then
+  if command -v apt-get &> /dev/null; then
     echo "This is Ubuntu. Using dpkg."
     # install packages, including vim8 from ppa
     sudo add-apt-repository ppa:jonathonf/vim
@@ -33,12 +33,12 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] ; then
     sudo apt-get install build-essential libgtest-dev curl terminator git python vim safe-rm openssh-server tmux vim trash-cli
 
   # OpenSuse, Mandriva, Fedora, CentOs, ecc. (with rpm)
-  elif which rpm &> /dev/null; then
+  elif command -v rpm &> /dev/null; then
     echo "This is Red Hat / CentOS. Using rpm."
     sudo yum install rh-python36 build-essential libgtest-dev curl terminator git python safe-rm openssh-server tmux vim trash-cli
 
   # ArchLinux (with pacman)
-  elif which pacman &> /dev/null; then
+  elif command -v pacman &> /dev/null; then
     echo "This is ArchLinux. Using pacman."
     pacman -S python build-essential libgtest-dev curl terminator git vim safe-rm openssh-server tmux trash-cli
   else
@@ -48,26 +48,17 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] ; then
 fi
 
 # vim pathogen
-mkdir -p $HOME/.vim/autoload ~/.vim/bundle
-curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p "$HOME/.vim/autoload ~/.vim/bundle"
+curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
 
 # create folders if they dont exist
-if [ ! -d $HOME"/.vim" ]; then
-  mkdir -p $HOME/.vim
+if [ ! -d "$HOME/.vim" ]; then
+  mkdir -p "$HOME/.vim"
 fi
-if [ ! -d $HOME"/.vim/pack/crich/start/vim-airline" ]; then
-git clone https://github.com/vim-airline/vim-airline $HOME/.vim/pack/dist/start/vim-airline
-vim -u NONE -c "helptags vim-airline/doc" -c q
-fi
-if [ ! -d $HOME"/.vim/bundle/vim-airline-themes" ]; then
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-vim -u NONE -c "helptags vim-airline-themes/doc" -c q
-fi
-
-if [ ! -d $HOME"/.dircolors" ]; then
-  mkdir -p $HOME/.dircolors
+if [ ! -d "$HOME/.dircolors" ]; then
+  mkdir -p "$HOME/.dircolors"
 fi
 
 # run python setup
 ScriptDir="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )";
-python $ScriptDir/config.py --all
+python "$ScriptDir/config.py" --all
